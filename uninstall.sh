19419dd876542db26b5d3e46ba68f9bdc2ef1a07
Lidia Parrilla <lidia.parrilla@idener.es>
2023-06-05 15:27:14 +0200

    Removing git aliases on uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,5 @@
+git config --global --unset alias.session-create
+git config --global --unset alias.session-start
 sudo rm /usr/local/bin/git-session-start
 sudo rm /usr/local/bin/git-session-create
 rm -r ~/.ssh/session-configs
